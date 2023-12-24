8a686333209869c53606a8fab9cf5f7ac947e2bb
Tri Nguyen <tringuyenminh23@gmail.com>
2023-12-24 11:19:51 +0200

    add prompt output key

--- src/prompts.py
+++ src/prompts.py
@@ -7,62 +7,78 @@ LOGGER = get_logger(__name__)
 
 
 class Prompt(ABC):
-    text: str
+    template: str
 
     @property
     @abstractmethod
     def input_keys(self) -> list[str]:
         pass
 
-    def format_input(self, inputs: dict[str, Any]) -> str:
+    @property
+    @abstractmethod
+    def output_key(self) -> str | None:
+        pass
+
+    def format_inputs(self, inputs: dict[str, Any]) -> str:
         input_keys = set(inputs.keys())
         prompt_input_keys = set(self.input_keys)
         if not prompt_input_keys.issubset(input_keys):
             raise KeyError(f"Missing required input keys: {prompt_input_keys - input_keys}")
         try:
-            return self.text.format(**{k: inputs[k] for k in self.input_keys})
+            return self.template.format(**{k: inputs[k] for k in self.input_keys})
         except Exception as e:
-            LOGGER.exception("Failed to format prompt %s with value %s: %s", self.text, inputs, e)
+            LOGGER.exception("Failed to format prompt %s with value %s: %s", self.template, inputs, e)
             raise
 
 
 class QAPrompt(Prompt):
-    CONTEXT_KEY = "context"
-    QUESTION_KEY = "question"
+    INPUT_CONTEXT_KEY = "context"
+    INPUT_QUESTION_KEY = "question"
+    PROMPT_OUTPUT_KEY = "Output:"
 
-    text = f"""Instruct: Use the following pieces of context to answer the question at the end.
- If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    template = f"""Instruct: Given the context below:
+{{{INPUT_CONTEXT_KEY}}}
 
-Context: {{{CONTEXT_KEY}}}
+Give a precise answer to the following question
 
-Question: {{{QUESTION_KEY}}}
+Question: {{{INPUT_QUESTION_KEY}}}
 
-Output:
+{PROMPT_OUTPUT_KEY}
 """
 
     @property
     def input_keys(self) -> list[str]:
-        return [self.CONTEXT_KEY, self.QUESTION_KEY]
+        return [self.INPUT_CONTEXT_KEY, self.INPUT_QUESTION_KEY]
+
+    @property
+    def output_key(self) -> str:
+        return self.PROMPT_OUTPUT_KEY
 
 
 class EvaluationPrompt(Prompt):
-    QUESTION_KEY = "question"
-    ANSWER_KEY = "answer"
-    REFERENCE_ANSWER_KEY = "reference"
+    INPUT_QUESTION_KEY = "question"
+    INPUT_ANSWER_KEY = "answer"
+    INPUT_REFERENCE_ANSWER_KEY = "reference"
+    PROMPT_OUTPUT_KEY = "Output:"
 
-    text = f"""Instruct: Evaluate the correctness of the answer of a given question compared to the reference answer.
+    template = f"""\
+Instruct: Evaluate the correctness of the answer of a given question compared to the reference answer.
 
-Question: {{{QUESTION_KEY}}}
+Question: {{{INPUT_QUESTION_KEY}}}
 
-Answer: {{{ANSWER_KEY}}}
+Answer: {{{INPUT_ANSWER_KEY}}}
 
-Reference Answer: {{{REFERENCE_ANSWER_KEY}}}
+Reference Answer: {{{INPUT_REFERENCE_ANSWER_KEY}}}
 
 Write out the reasoning of the evaluation and finally answer "CORRECT" or "INCORRECT".
 
-Output:
+{PROMPT_OUTPUT_KEY}
 """
 
     @property
     def input_keys(self) -> list[str]:
-        return [self.QUESTION_KEY, self.ANSWER_KEY, self.REFERENCE_ANSWER_KEY]
+        return [self.INPUT_QUESTION_KEY, self.INPUT_ANSWER_KEY, self.INPUT_REFERENCE_ANSWER_KEY]
+
+    @property
+    def output_key(self) -> str:
+        return self.PROMPT_OUTPUT_KEY
--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -7,11 +7,11 @@ def test_qa_prompt_format() -> None:
     qa_prompt = QAPrompt()
     context, question = "Some context", "Some question"
 
-    formatted_prompt = qa_prompt.format_input(inputs={"context": context, "question": question})
+    formatted_prompt = qa_prompt.format_inputs(inputs={"context": context, "question": question})
     assert context in formatted_prompt, f"Expected formatted prompt {formatted_prompt} to have context: {context}"
     assert question in formatted_prompt, f"Expected formatted prompt {formatted_prompt} to have question: {question}"
 
 
 def test_invalid_qa_prompt_format() -> None:
     with pytest.raises(KeyError):
-        QAPrompt().format_input(inputs={"incorrect": "prompt"})
+        QAPrompt().format_inputs(inputs={"incorrect": "prompt"})
